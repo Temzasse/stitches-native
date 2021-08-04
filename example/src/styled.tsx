a5b8c4f07ea1bd7148aaebae11f09dd0312adc38
Cesar Santos <rasec_1113@hotmail.com>
2021-08-04 18:03:16 -0500

    run lint

--- example/src/styled.tsx
+++ example/src/styled.tsx
@@ -70,7 +70,7 @@ const { styled, css, theme, ThemeProvider, useTheme } = createCss({
     }),
     absoluteFill: () => () => ({
       ...StyleSheet.absoluteFillObject,
-    })
+    }),
   },
   media,
   // media: {
