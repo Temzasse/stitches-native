f9f0b2092db0488b4021a5abf95c9c717eac9121
Teemu Taskula <themetor@gmail.com>
2022-06-05 16:25:42 +0300

    Update typescript to 4.7.3 and format code

--- src/types/stitches.d.ts
+++ src/types/stitches.d.ts
@@ -145,9 +145,9 @@ export default interface Stitches<
         [K in keyof Composers]: string extends Composers[K] // Strings and Functions can be skipped over
           ? Composers[K]
           : Composers[K] extends
-          | string
-          | React.ComponentType<any>
-          | Util.Function
+              | string
+              | React.ComponentType<any>
+              | Util.Function
           ? Composers[K]
           : CSS & {
               /** The **variants** property lets you set a subclass of styles based on a key-value pair.
