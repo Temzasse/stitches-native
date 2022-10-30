496a0bfff50a533f09f0d74ba034fc449291e356
tkow <t.kusama0613@outlook.jp>
2022-10-31 00:26:07 +0900

    fix: improvement dev config

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -5,6 +5,10 @@ const path = require('path');
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   // If you want to add a new alias to the config.
-  config.resolve.alias['react'] = path.join(__dirname, 'node_modules', 'react');
+  Object.assign(config.resolve.alias, {
+    react: path.join(__dirname, 'node_modules', 'react'),
+    'react-native': path.join(__dirname, 'node_modules', 'react-native-web'),
+    'react-native-web': path.join(__dirname, 'node_modules', 'react-native-web'), // prettier-ignore
+  });
   return config;
 };
