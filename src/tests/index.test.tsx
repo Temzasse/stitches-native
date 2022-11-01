aea62204b6ffd8a23b8bbf3f78d14c03c57b4d6d
Teemu Taskula <themetor@gmail.com>
2022-11-01 10:40:01 +0200

    Fix test type issue

--- src/tests/index.test.tsx
+++ src/tests/index.test.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import { createStitches } from '../internals';
+import { createStitches as _createStitches } from '../internals';
+import type { CreateStitches } from '../types';
+
+// NOTE: the JSDoc types from the internal `createStitches` are not working properly
+const createStitches = _createStitches as CreateStitches;
 
 describe('Basic', () => {
   it('Functionality of styled()', () => {
@@ -15,8 +19,8 @@ describe('Basic', () => {
     const { toJSON } = render(<Comp />);
     const result = toJSON();
 
-    expect(result.type).toEqual('View');
-    expect(result.props.style[0]).toMatchObject({
+    expect(result?.type).toEqual('View');
+    expect(result?.props.style[0]).toMatchObject({
       backgroundColor: 'red',
       height: 100,
       width: 100,
