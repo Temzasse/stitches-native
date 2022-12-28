e04c57c8e5e2cf9aa0439a93ef7c41f65319de9c
Teemu Taskula <themetor@gmail.com>
2022-12-28 22:27:22 +0200

    Mock pixel ratio

--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -3,13 +3,24 @@ const aspectRatio = 19.5 / 9; // iPhone 14
 export function mockDimensions({
   width = 750,
   height = width * aspectRatio,
+  pixelRatio = 1,
 }: {
   width?: number;
   height?: number;
+  pixelRatio?: number;
 }) {
   jest.resetModules();
   jest.doMock('react-native/Libraries/Utilities/useWindowDimensions', () => ({
     __esModule: true,
     default: jest.fn().mockReturnValue({ width, height }),
   }));
+  jest.doMock('react-native/Libraries/Utilities/PixelRatio', () => ({
+    get: () => pixelRatio,
+    getFontScale: () => 1,
+    getPixelSizeForLayoutSize: (layoutSize: number) => layoutSize * pixelRatio,
+    roundToNearestPixel: (layoutSize: number) =>
+      Math.round(layoutSize * pixelRatio) / pixelRatio,
+  }));
+}
+
 }
